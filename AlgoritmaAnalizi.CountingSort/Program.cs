1999e3b23ebfdb097a89167dc838031b6a9dcf4c
fehmiaksakal <fehmiaksakal01@gmail.com>
2020-11-14 00:20:51 +0300

    a

---
+++ AlgoritmaAnalizi.CountingSort/Program.cs
@@ -0,0 +1,61 @@
+﻿using AlgoritmaAnalizi.Core;
+using System;
+
+namespace AlgoritmaAnalizi.CountingSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[2] { 0, 1 };//Helper.CreateAnArray(15, 50, 20);
+
+            Sort(arr);
+
+            Helper.WriteArray(arr, "Sorted Array");
+
+            Console.ReadLine();
+        }
+
+        static void Sort(int[] array)
+        {
+            int length = array.Length;
+
+            //Create a new "output" array
+            int[] output = new int[length];
+
+            //Create a new "counting" array 
+            //which stores the count of each unique number
+            int[] count = new int[100];
+            for (int i = 0; i < 100; ++i)
+            {
+                count[i] = 0;
+            }
+            for (int i = 0; i < length; ++i)
+            {
+                ++count[array[i]];
+            }
+
+            //Change count[i] so that count[i] now contains the   
+            //actual position of this character in the output array.
+            for (int i = 1; i <= 99; ++i)
+            {
+                count[i] += count[i - 1];
+            }
+
+            //Build the output array.
+            //To make this sorting algorithm stable, 
+            //we are operating in reverse order. 
+            for (int i = length - 1; i >= 0; i--)
+            {
+                output[count[array[i]] - 1] = array[i];
+                --count[array[i]];
+            }
+
+            //Copy the output array to the final array.
+            for (int i = 0; i < length; ++i)
+            {
+                array[i] = output[i];
+            }
+        }
+    }
+}
---
+++ AlgoritmaAnalizi.RandomizedQuickSort/Program.cs
@@ -0,0 +1,60 @@
+﻿using AlgoritmaAnalizi.Core;
+using System;
+
+namespace AlgoritmaAnalizi.RandomizedQuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Helper.CreateAnArray(15, 50, 20);
+
+            RandomizedQuickSort(arr, 0, arr.Length - 1);
+
+            Helper.WriteArray(arr, desc: "Sorted Array");
+        }
+        public static void RandomizedQuickSort(int[] input, int left, int right)
+        {
+            if (left < right)
+            {
+                int q = RandomizedPartition(input, left, right);
+                RandomizedQuickSort(input, left, q - 1);
+                RandomizedQuickSort(input, q + 1, right);
+            }
+        }
+        private static int RandomizedPartition(int[] input, int left, int right)
+        {
+            Random random = new Random();
+            int i = random.Next(left, right);
+
+            int pivot = input[i];
+            input[i] = input[right];
+            input[right] = pivot;
+
+            return Partition(input, left, right);
+        }
+        private static int Partition(int[] input, int left, int right)
+        {
+            int pivot = input[right];
+            int temp;
+
+            int i = left;
+            for (int j = left; j < right; j++)
+            {
+                if (input[j] <= pivot)
+                {
+                    temp = input[j];
+                    input[j] = input[i];
+                    input[i] = temp;
+                    i++;
+                }
+            }
+
+            input[right] = input[i];
+            input[i] = pivot;
+
+            return i;
+        }
+    }
+
+}
